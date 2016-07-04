236a1ecba03a438e653394d796e1dda2fcb87c49
B-VAN <the1437@gmail.com>
2016-07-04 20:41:36 +1000

    Grid filler

---
+++ grid.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <iomanip>
+
+#include <cmath>
+
+#define COND 2
+
+int m_create(std::vector<int> input, int size) {
+	int row = pow(size, 6);
+	int col = pow(size, 4)*COND;
+	int m[row][col];
+	std::vector<int>::iterator v = input.begin();
+	for (int p = row; p < row; ++p) {
+		for (int q = col; q < col; ++q) {
+			m[p][q] = *v++;
+		}
+	}
+	(void)m;//compile
+	return 0;
+}
